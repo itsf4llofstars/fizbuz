3846c4a2fc0a7d7da4c75f7e47ff17305ababe56
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-06 00:58:41 -0500

    Add increase_number function

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+"""fizzbuzz.py"""
+
+
+def increase_number(number: int, value: int = 1) -> int:
+    """Increase the value of a passed number.
+
+    Args:
+        number (int): The number to be increased
+        value (int): The amount to increas the number defaulte: 1
+
+    Returns:
+        (int): The number increased by the value
+    """
+    return number + value
