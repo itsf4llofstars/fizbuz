82445505c575af341d8a235e43ca959646eea992
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-09 18:06:51 -0500

    Add finally to return if a list is created

--- main.py
+++ main.py
@@ -20,10 +20,15 @@ def read_full_file(filename: str):
     else:
         for line in lines:
             file_lines.append(line.rstrip())
+    finally:
+        if file_lines:
+            return file_lines
 
 
 def main():
-    pass
+    file_name = os.path.expanduser(os.path.join("~", "ed", "file.txt"))
+    files_lines = read_full_file(file_name)
+    [print(line) for line in files_lines]
 
 
 if __name__ == "__main__":
