7113a3062ca73ba5f3cb1a0354f4e491e719dc2f
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-09 18:00:54 -0500

    Add doc to read_full_file

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+import os
+
+
+def read_full_file():
+    """Read in an entire file and return each line in a list
+
+    Args:
+        filename (str): The path and filename to be read
+
+    Returns (List[str]): A list of string each being one
+                         line of the read file
+    """
+    pass
 
 
 def main():
