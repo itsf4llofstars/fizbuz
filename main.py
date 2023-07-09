71414220e55faab1be033f8393f0925ab58c6733
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-08 22:38:06 -0500

    Add housekeeping and line printing

--- main.py
+++ main.py
@@ -3,31 +3,40 @@ import os
 
 
 def read_full_file(filename: str):
-    """Read in an entire file and return each line in a list
+    """Read in an entire file and return each line as a list
 
     Args:
         filename (str): The path and filename to be read
 
-    Returns (List[str]): A list of string each being one
-                         line of the read file
+    Returns
+        (List[str]): A list of string each being one line
+                     of the read file
     """
+
     file_lines = []
+
     try:
         with open(filename, encoding="utf-8") as read:
             lines = read.readlines()
     except FileNotFoundError as fnfe:
-        print(f"ERROR: {fnfe}")
+        raise fnfe
     else:
         for line in lines:
             file_lines.append(line.rstrip())
-    finally:
-        if file_lines:
-            return file_lines
+
+    return file_lines
 
 
 def main():
+    """main"""
     file_name = os.path.expanduser(os.path.join("~", "ed", "file.txt"))
     files_lines = read_full_file(file_name)
+
+    for line in files_lines:
+        print(line)
+
+    print()
+
     [print(line) for line in files_lines]
 
 
