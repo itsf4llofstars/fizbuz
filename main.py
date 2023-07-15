a4684c78680d1f9d055012096af1181a24816a51
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-14 22:08:23 -0500

    Add read_file() function

--- main.py
+++ main.py
@@ -3,6 +3,21 @@
 import os
 
 
+def read_file(filename: str) -> object:
+    file_lines = []
+
+    try:
+        with open(filename, encoding="utf-8") as read:
+            raw_lines = read.readlines()
+    except FileNotFoundError as fnfe:
+        raise fnfe
+    else:
+        for line in raw_lines:
+            file_lines.append(line.rstrip())
+
+    return file_lines
+
+
 def main() -> None:
     """main"""
     print("Hello, world!")
