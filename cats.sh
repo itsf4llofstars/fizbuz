d4647ec12b982a0b084e5515a8502f5fbf9b0acd
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-14 21:44:35 -0500

    Add % in front of UTC offset

--- cats.sh
+++ cats.sh
@@ -2,6 +2,6 @@
 # cats.sh
 # A script to print commonly cat'd textfiles
 
-printf "%s/\n" "Hello, world! today is: $(date +'%FT%T :::z')Z - $0" 2>/dev/null
+printf "%s/\n" "Hello, world! today is: $(date +'%FT%T %:::z')Z - $0" 2>/dev/null
 
 exit 0
