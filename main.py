ec728399ac30d7757bc67640cd0f4be03a596b34
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-14 21:41:06 -0500

    Instantiate main.py with header code

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""main.py"""
+import os
+import sys
+
+
+def main(args) -> None:
+    """main"""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    os.system("clear")
+
+    sys.exit(main(sys.argv))
