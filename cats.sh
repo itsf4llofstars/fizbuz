7bdc95936fe493496ac551d7d27fc347ceb147a8
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-27 14:26:48 -0500

    Add use doc cats.sh

--- cats.sh
+++ cats.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # cats.sh
 # A script to print commonly cat'd textfiles
+# Works well by adding a symbolic link to ~/bin as
+# cats.
 
 printf "%s/\n" "Hello, world! today is: $(date +'%FT%T %:::z')Z - $0" 2>/dev/null
 
